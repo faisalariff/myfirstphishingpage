c1b3980cb04931fc1a760dbdc9fbcf68163d0ccf
codedivision <user@codedivision.my>
2015-05-27 11:16:47 +0800

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,19 @@
+get '/' do
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @user_input = params["user_input"]
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @user_input = params["user_input"]
+  erb :post_cool_url
+end
